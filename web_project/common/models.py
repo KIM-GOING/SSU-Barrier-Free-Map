95af8c146331309a0fddd7b70f68f8419d6107e0
Sy P <yongsa0221@gmail.com>
2022-11-01 01:03:21 +0900

    db

--- web_project/common/models.py
+++ web_project/common/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
+    latitude = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+class BarrierFreeInfo(models.Model):
+    entrance = models.CharField(max_length=200)
+    slope = models.CharField(max_length=200)
+    is_accessible_toilet = models.BooleanField()
+    is_braille = models.BooleanField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
