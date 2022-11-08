4ea6ea69b01deb8f357802490c34c2db8e173c0c
Sy P <yongsa0221@gmail.com>
2022-11-08 18:36:50 +0900

    campus_updated

--- web_project/campus/urls.py
+++ web_project/campus/urls.py
@@ -5,5 +5,7 @@ from. import views
 app_name = 'campus'
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:campus_id>/', views.detail, name='detail')
 
 ]
--- web_project/common/models.py
+++ web_project/common/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-
+from config import settings
+from datetime import datetime
+import os
+from uuid import uuid4
 # Create your models here.
+def get_file_path(instance, filename):
+    ymd_path = datetime.now().strftime('%Y/%m/%d')
+    ext = os.path.splitext(filename)[-1].lower()
+    uuid_name = uuid4().hex
+    return '/'.join(['common/', ymd_path, uuid_name+ext])
+
 class Location(models.Model):
     name = models.CharField(max_length=200)
     longitude = models.FloatField()
@@ -10,19 +19,27 @@ class Location(models.Model):
 class BarrierFreeInfo(models.Model):
     elevator_count = models.IntegerField(null=True)
     elevator_detail = models.TextField(null=True)
-    elevator_img = models.ImageField(null=True)
+    elevator_img = models.ImageField(null=True , upload_to=get_file_path)
 
     entrance_form = models.CharField(max_length=200)
     entrance_ramp = models.CharField(max_length=200)
     entrance_braille = models.BooleanField(null=True)
     entrance_detail = models.TextField(blank=True)
-    entrance_img = models.ImageField(null=True)
+    entrance_img = models.ImageField(null=True , upload_to=get_file_path)
 
     restroom_floor = models.IntegerField(null=True)
     restroom_detail = models.TextField(blank=True)
-    restroom_image = models.ImageField(null=True)
+    restroom_image = models.ImageField(null=True , upload_to=get_file_path)
 
     parking_count = models.IntegerField(null=True)
     parking_detail = models.TextField(blank=True)
-    parking_img = models.ImageField(null=True)
+    parking_img = models.ImageField(null=True , upload_to=get_file_path)
+
+    def delete(self, *args, **kwargs):
+        super(BarrierFreeInfo, self).delete(*args, **kwargs)
+        os.remove(os.path.join(settings.MEDIA_ROOT, self.elevator_img.path))
+        os.remove(os.path.join(settings.MEDIA_ROOT, self.entrance_img.path))
+        os.remove(os.path.join(settings.MEDIA_ROOT, self.restroom_image.path))
+        os.remove(os.path.join(settings.MEDIA_ROOT, self.parking_img.path))
+
 
